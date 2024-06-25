5d36eb64f46fa11662110ea0595a199fe7fa144a
ltitanb <lorenzo@gattaca.com>
2024-06-25 20:00:41 +0100

    temp add helder devnet values

--- crates/common/src/types.rs
+++ crates/common/src/types.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
 
 use crate::constants::{
+    HELDER_BUILDER_DOMAIN, HELDER_FORK_VERSION, HELDER_GENESIS_TIME_SECONDS,
     HOLESKY_BUILDER_DOMAIN, HOLESKY_FORK_VERSION, HOLESKY_GENESIS_TIME_SECONDS,
     MAINNET_BUILDER_DOMAIN, MAINNET_FORK_VERSION, MAINNET_GENESIS_TIME_SECONDS,
     RHEA_BUILDER_DOMAIN, RHEA_FORK_VERSION, RHEA_GENESIS_TIME_SECONDS,
@@ -11,6 +12,7 @@ pub enum Chain {
     Mainnet,
     Holesky,
     Rhea,
+    Helder,
 }
 
 impl Chain {
@@ -19,6 +21,7 @@ impl Chain {
             Chain::Mainnet => MAINNET_BUILDER_DOMAIN,
             Chain::Holesky => HOLESKY_BUILDER_DOMAIN,
             Chain::Rhea => RHEA_BUILDER_DOMAIN,
+            Chain::Helder => HELDER_BUILDER_DOMAIN,
         }
     }
 
@@ -27,6 +30,7 @@ impl Chain {
             Chain::Mainnet => MAINNET_FORK_VERSION,
             Chain::Holesky => HOLESKY_FORK_VERSION,
             Chain::Rhea => RHEA_FORK_VERSION,
+            Chain::Helder => HELDER_FORK_VERSION,
         }
     }
 
@@ -35,6 +39,7 @@ impl Chain {
             Chain::Mainnet => MAINNET_GENESIS_TIME_SECONDS,
             Chain::Holesky => HOLESKY_GENESIS_TIME_SECONDS,
             Chain::Rhea => RHEA_GENESIS_TIME_SECONDS,
+            Chain::Helder => HELDER_GENESIS_TIME_SECONDS,
         }
     }
 }
