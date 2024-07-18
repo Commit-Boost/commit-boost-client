ebe98b557ef1acd28bcd6fce523a63277f93d04d
ltitanb <lorenzo@gattaca.com>
2024-07-18 16:39:50 +0100

    swtich to node

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://commit-boost.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/commit-boost-client/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
