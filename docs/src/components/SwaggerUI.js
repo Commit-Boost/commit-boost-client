b4464545882c841d844e7f67de28efb1b8655d3a
ltitanb <lorenzo@gattaca.com>
2024-07-18 20:59:31 +0100

    api folder

--- docs/src/components/SwaggerUI.js
+++ docs/src/components/SwaggerUI.js
@@ -3,7 +3,7 @@ import SwaggerUI from 'swagger-ui-react';
 import 'swagger-ui-react/swagger-ui.css';
 
 const SwaggerUIComponent = () => {
-    return <SwaggerUI url="/signer-api.yml" />;
+    return <SwaggerUI url="https://raw.githubusercontent.com/Commit-Boost/commit-boost-client/main/api/signer-api.yml" />;
 };
 
 export default SwaggerUIComponent;
