6c53f23577c1b46ecfb6e4f0b14b9d7996c93a99
David Petrov <david.petrov@limechain.tech>
2024-08-05 16:51:56 +0300

    chore(signer): remove `SignerModuleError::UnknownModuleId`
    not needed anymore since JWTs now uniquely identify the module

--- crates/signer/src/error.rs
+++ crates/signer/src/error.rs
@@ -3,16 +3,12 @@ use axum::{
     http::StatusCode,
     response::{IntoResponse, Response},
 };
-use cb_common::types::ModuleId;
 use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum SignerModuleError {
     #[error("unauthorized")]
     Unauthorized,
-
-    #[error("unknown module id: {0}")]
-    UnknownModuleId(ModuleId),
 
     #[error("unknown consensus signer: {0}")]
     UnknownConsensusSigner(BlsPublicKey),
@@ -25,7 +21,6 @@ impl IntoResponse for SignerModuleError {
     fn into_response(self) -> Response {
         let status = match self {
             SignerModuleError::Unauthorized => StatusCode::UNAUTHORIZED,
-            SignerModuleError::UnknownModuleId(_) => StatusCode::NOT_FOUND,
             SignerModuleError::UnknownConsensusSigner(_) => StatusCode::NOT_FOUND,
             SignerModuleError::UnknownProxySigner(_) => StatusCode::NOT_FOUND,
         };
