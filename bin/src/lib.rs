60d324409ab10b5eacb4c55db763dfc3dfdf9f02
ltitanb <lorenzo@gattaca.com>
2024-08-06 17:56:57 +0100

    simplify `BuilderApi` and exports

--- bin/src/lib.rs
+++ bin/src/lib.rs
@@ -7,7 +7,10 @@ pub mod prelude {
         utils::{initialize_tracing_log, utcnow_ms, utcnow_ns, utcnow_sec, utcnow_us},
     };
     pub use cb_metrics::provider::MetricsProvider;
-
+    pub use cb_pbs::{
+        get_header, get_status, register_validator, submit_block, BuilderApi, BuilderApiState,
+        PbsState,
+    };
     // The TreeHash derive macro requires tree_hash:: as import
     pub mod tree_hash {
         pub use tree_hash::*;
