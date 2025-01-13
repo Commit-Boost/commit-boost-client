4e08196fafb51f3c3f4138f132172332df08b318
Manuel Iñaki Bilbao <manuel.bilbao@lambdaclass.com>
2025-01-13 15:40:47 -0300

    Add function doc

--- crates/pbs/src/mev_boost/reload.rs
+++ crates/pbs/src/mev_boost/reload.rs
@@ -2,6 +2,8 @@ use cb_common::config::load_pbs_config;
 
 use crate::{BuilderApiState, InnerPbsState, PbsState};
 
+/// Reload the PBS state with the latest configuration in the config file
+/// Returns 200 if successful or 500 if failed
 pub async fn reload<S: BuilderApiState>(state: PbsState<S>) -> eyre::Result<()> {
     let pbs_config = load_pbs_config().await?;
     let new_state = InnerPbsState::new(pbs_config).with_data(state.inner.read().await.data.clone());
