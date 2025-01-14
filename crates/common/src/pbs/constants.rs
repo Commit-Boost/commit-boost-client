c6f5995ca54b3e624b848707d0372a6710e9b85c
nicolas <48695862+merklefruit@users.noreply.github.com>
2025-01-14 14:41:38 +0100

    chore: bump axum (#232)
    Co-authored-by: ltitanb <lorenzo@gattaca.com>

--- crates/common/src/pbs/constants.rs
+++ crates/common/src/pbs/constants.rs
@@ -2,7 +2,7 @@ use crate::constants::COMMIT_BOOST_VERSION;
 
 pub const BUILDER_API_PATH: &str = "/eth/v1/builder";
 
-pub const GET_HEADER_PATH: &str = "/header/:slot/:parent_hash/:pubkey";
+pub const GET_HEADER_PATH: &str = "/header/{slot}/{parent_hash}/{pubkey}";
 pub const GET_STATUS_PATH: &str = "/status";
 pub const REGISTER_VALIDATOR_PATH: &str = "/validators";
 pub const SUBMIT_BLOCK_PATH: &str = "/blinded_blocks";
