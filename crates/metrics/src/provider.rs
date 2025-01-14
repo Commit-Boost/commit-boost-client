7fa6838ee2033c757eee4370d61885c531e6d9c7
ltitanb <163874448+ltitanb@users.noreply.github.com>
2025-01-14 22:02:45 +0000

    feat: add info metrics (#234)
    Co-authored-by: Manuel Iñaki Bilbao <bilbaomanuel98@gmail.com>

--- crates/metrics/src/provider.rs
+++ crates/metrics/src/provider.rs
@@ -7,28 +7,33 @@ use axum::{
     response::{IntoResponse, Response},
     routing::get,
 };
-use cb_common::config::ModuleMetricsConfig;
+use cb_common::{
+    config::ModuleMetricsConfig,
+    constants::{COMMIT_BOOST_COMMIT, COMMIT_BOOST_VERSION},
+    types::Chain,
+};
 use eyre::bail;
-use prometheus::{Encoder, Registry, TextEncoder};
+use prometheus::{Encoder, IntGauge, Opts, Registry, TextEncoder};
 use tokio::net::TcpListener;
 use tracing::{error, info, trace, warn};
 
 pub struct MetricsProvider {
+    network: Chain,
     config: ModuleMetricsConfig,
     registry: Registry,
 }
 
 impl MetricsProvider {
-    pub fn new(config: ModuleMetricsConfig, registry: Registry) -> Self {
-        MetricsProvider { config, registry }
+    pub fn new(network: Chain, config: ModuleMetricsConfig, registry: Registry) -> Self {
+        MetricsProvider { network, config, registry }
     }
 
-    pub fn from_registry(registry: Registry) -> eyre::Result<Option<Self>> {
-        Ok(ModuleMetricsConfig::load_from_env()?.map(|config| MetricsProvider { config, registry }))
+    pub fn from_registry(network: Chain, registry: Registry) -> eyre::Result<Option<Self>> {
+        Ok(ModuleMetricsConfig::load_from_env()?.map(|config| Self::new(network, config, registry)))
     }
 
-    pub fn load_and_run(registry: Registry) -> eyre::Result<()> {
-        if let Some(provider) = MetricsProvider::from_registry(registry)? {
+    pub fn load_and_run(network: Chain, registry: Registry) -> eyre::Result<()> {
+        if let Some(provider) = MetricsProvider::from_registry(network, registry)? {
             tokio::spawn(async move {
                 if let Err(err) = provider.run().await {
                     error!("Metrics server error: {:?}", err);
@@ -43,6 +48,15 @@ impl MetricsProvider {
 
     pub async fn run(self) -> eyre::Result<()> {
         info!("Starting metrics server on port {}", self.config.server_port);
+
+        let opts = Opts::new("info", "Commit Boost info")
+            .const_label("version", COMMIT_BOOST_VERSION)
+            .const_label("commit", COMMIT_BOOST_COMMIT)
+            .const_label("network", self.network.to_string());
+        let info = IntGauge::with_opts(opts).unwrap();
+        info.set(1);
+
+        self.registry.register(Box::new(info)).unwrap();
 
         let router = axum::Router::new()
             .route("/metrics", get(handle_metrics))
