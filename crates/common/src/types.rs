471c6289168c637dc116a9050a965c95a7e1bb3c
ltitanb <163874448+ltitanb@users.noreply.github.com>
2025-01-14 21:33:07 +0000

    Update crates/common/src/types.rs
    Co-authored-by: Manuel Iñaki Bilbao <bilbaomanuel98@gmail.com>

--- crates/common/src/types.rs
+++ crates/common/src/types.rs
@@ -31,7 +31,7 @@ pub type ForkVersion = [u8; 4];
 impl std::fmt::Display for Chain {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::Mainnet | Self::Holesky | Self::Sepolia | Self::Helder => write!(f, "{:?}", self),
+            Self::Mainnet | Self::Holesky | Self::Sepolia | Self::Helder => write!(f, "{self:?}"),
             Self::Custom { .. } => write!(f, "Custom"),
         }
     }
