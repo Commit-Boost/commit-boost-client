2c8d2f8fd972a42604a3c7fb0655a52477004e78
JasonVranek <jasonvranek@gmail.com>
2025-02-11 12:02:16 -0800

    add new Electra structs for BlindedBeaconBlockBody
    - ExecutionRequests
    - DepositRequest
    - WithdrawalRequest
    - ConsolidationRequest

--- crates/common/src/pbs/types/blinded_block_body.rs
+++ crates/common/src/pbs/types/blinded_block_body.rs
@@ -152,3 +152,34 @@ pub struct SyncAggregate<T: EthSpec> {
     pub sync_committee_bits: BitVector<T::SyncCommitteeSize>,
     pub sync_committee_signature: BlsSignature,
 }
+
+#[derive(Debug, Default, Clone, Serialize, Deserialize)]
+#[serde(bound = "T: EthSpec")]
+pub struct ExecutionRequests<T: EthSpec> {
+    pub deposits: VariableList<DepositRequest, T::MaxDepositRequestsPerPayload>,
+    pub withdrawals: VariableList<WithdrawalRequest, T::MaxWithdrawalRequestsPerPayload>,
+    pub consolidations: VariableList<ConsolidationRequest, T::MaxConsolidationRequestsPerPayload>,
+}
+
+#[derive(Debug, Default, Clone, Serialize, Deserialize)]
+pub struct DepositRequest {
+    pub pubkey: BlsPublicKey,
+    pub withdrawal_credentials: B256,
+    pub amount: u64,
+    pub signature: BlsSignature,
+    pub index: u64,
+}
+
+#[derive(Debug, Default, Clone, Serialize, Deserialize)]
+pub struct WithdrawalRequest {
+    pub source_address: Address,
+    pub validator_pubkey: BlsPublicKey,
+    pub amount: u64,
+}
+
+#[derive(Debug, Default, Clone, Serialize, Deserialize)]
+pub struct ConsolidationRequest {
+    pub source_address: Address,
+    pub source_pubkey: BlsPublicKey,
+    pub target_pubkey: BlsPublicKey,
+}
