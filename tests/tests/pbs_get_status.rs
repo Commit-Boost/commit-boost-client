307365920ba92fb8c4fe914cc8be77fc3b3656a6
JasonVranek <jasonvranek@gmail.com>
2025-02-12 11:31:00 -0800

    fix pbs_get_status.rs tests

--- tests/tests/pbs_get_status.rs
+++ tests/tests/pbs_get_status.rs
@@ -1,85 +1,155 @@
-// use std::{sync::Arc, time::Duration};
+use std::{sync::Arc, time::Duration};
 
-// use cb_common::{
-//     signer::{random_secret, BlsPublicKey},
-//     types::Chain,
-//     utils::blst_pubkey_to_alloy,
-// };
-// use cb_pbs::{DefaultBuilderApi, PbsService, PbsState};
-// use cb_tests::{
-//     mock_relay::{start_mock_relay_service, MockRelayState},
-//     mock_validator::MockValidator,
-//     utils::{generate_mock_relay, get_pbs_static_config, setup_test_env, to_pbs_config},
-// };
-// use eyre::Result;
-// use reqwest::StatusCode;
-// use tracing::info;
+use cb_common::{
+    signer::{random_secret, BlsPublicKey},
+    types::Chain,
+    pbs::{DenebSpec, ElectraSpec},
+    utils::blst_pubkey_to_alloy,
+};
+use cb_pbs::{DefaultBuilderApi, PbsService, PbsState};
+use cb_tests::{
+    mock_relay::{start_mock_relay_service, MockRelayState},
+    mock_validator::MockValidator,
+    utils::{generate_mock_relay, get_pbs_static_config, setup_test_env, to_pbs_config},
+};
+use eyre::Result;
+use reqwest::StatusCode;
+use tracing::info;
 
-// #[tokio::test]
-// async fn test_get_status() -> Result<()> {
-//     setup_test_env();
-//     let signer = random_secret();
-//     let pubkey: BlsPublicKey = blst_pubkey_to_alloy(&signer.sk_to_pk()).into();
+#[tokio::test]
+async fn test_get_status_deneb() -> Result<()> {
+    setup_test_env();
+    let signer = random_secret();
+    let pubkey: BlsPublicKey = blst_pubkey_to_alloy(&signer.sk_to_pk()).into();
 
-//     let chain = Chain::Holesky;
-//     let pbs_port = 3500;
-//     let relay_0_port = pbs_port + 1;
-//     let relay_1_port = pbs_port + 2;
+    let chain = Chain::Holesky;
+    let pbs_port = 3500;
+    let relay_0_port = pbs_port + 1;
+    let relay_1_port = pbs_port + 2;
 
-//     let relays = vec![
-//         generate_mock_relay(relay_0_port, *pubkey)?,
-//         generate_mock_relay(relay_1_port, *pubkey)?,
-//     ];
-//     let mock_state = Arc::new(MockRelayState::new(chain, signer));
-//     tokio::spawn(start_mock_relay_service(mock_state.clone(), relay_0_port));
-//     tokio::spawn(start_mock_relay_service(mock_state.clone(), relay_1_port));
+    let relays = vec![
+        generate_mock_relay(relay_0_port, *pubkey)?,
+        generate_mock_relay(relay_1_port, *pubkey)?,
+    ];
+    let mock_state = Arc::new(MockRelayState::new(chain, signer));
+    tokio::spawn(start_mock_relay_service::<DenebSpec>(mock_state.clone(), relay_0_port));
+    tokio::spawn(start_mock_relay_service::<DenebSpec>(mock_state.clone(), relay_1_port));
 
-//     let config = to_pbs_config(chain, get_pbs_static_config(pbs_port), relays.clone());
-//     let state = PbsState::new(config);
-//     tokio::spawn(PbsService::run::<(), DefaultBuilderApi>(state));
+    let config = to_pbs_config(chain, get_pbs_static_config(pbs_port), relays.clone());
+    let state = PbsState::new(config);
+    tokio::spawn(PbsService::run::<(), DenebSpec, DefaultBuilderApi>(state));
 
-//     // leave some time to start servers
-//     tokio::time::sleep(Duration::from_millis(100)).await;
+    // leave some time to start servers
+    tokio::time::sleep(Duration::from_millis(100)).await;
 
-//     let mock_validator = MockValidator::new(pbs_port)?;
-//     info!("Sending get status");
-//     let res = mock_validator.do_get_status().await.expect("failed to get status");
-//     assert_eq!(res.status(), StatusCode::OK);
+    let mock_validator = MockValidator::new(pbs_port)?;
+    info!("Sending get status");
+    let res = mock_validator.do_get_status().await.expect("failed to get status");
+    assert_eq!(res.status(), StatusCode::OK);
 
-//     // Expect two statuses since two relays in config
-//     assert_eq!(mock_state.received_get_status(), 2);
-//     Ok(())
-// }
+    // Expect two statuses since two relays in config
+    assert_eq!(mock_state.received_get_status(), 2);
+    Ok(())
+}
 
-// #[tokio::test]
-// async fn test_get_status_returns_502_if_relay_down() -> Result<()> {
-//     setup_test_env();
-//     let signer = random_secret();
-//     let pubkey: BlsPublicKey = blst_pubkey_to_alloy(&signer.sk_to_pk()).into();
+#[tokio::test]
+async fn test_get_status_electra() -> Result<()> {
+    setup_test_env();
+    let signer = random_secret();
+    let pubkey: BlsPublicKey = blst_pubkey_to_alloy(&signer.sk_to_pk()).into();
 
-//     let chain = Chain::Holesky;
-//     let pbs_port = 3600;
-//     let relay_port = pbs_port + 1;
+    let chain = Chain::Holesky;
+    let pbs_port = 3550;
+    let relay_0_port = pbs_port + 1;
+    let relay_1_port = pbs_port + 2;
 
-//     let relays = vec![generate_mock_relay(relay_port, *pubkey)?];
-//     let mock_state = Arc::new(MockRelayState::new(chain, signer));
+    let relays = vec![
+        generate_mock_relay(relay_0_port, *pubkey)?,
+        generate_mock_relay(relay_1_port, *pubkey)?,
+    ];
+    let mock_state = Arc::new(MockRelayState::new(chain, signer));
+    tokio::spawn(start_mock_relay_service::<ElectraSpec>(mock_state.clone(), relay_0_port));
+    tokio::spawn(start_mock_relay_service::<ElectraSpec>(mock_state.clone(), relay_1_port));
 
-//     // Don't start the relay
-//     // tokio::spawn(start_mock_relay_service(mock_state.clone(), relay_port));
+    let config = to_pbs_config(chain, get_pbs_static_config(pbs_port), relays.clone());
+    let state = PbsState::new(config);
+    tokio::spawn(PbsService::run::<(), ElectraSpec, DefaultBuilderApi>(state));
 
-//     let config = to_pbs_config(chain, get_pbs_static_config(pbs_port), relays.clone());
-//     let state = PbsState::new(config);
-//     tokio::spawn(PbsService::run::<(), DefaultBuilderApi>(state));
+    // leave some time to start servers
+    tokio::time::sleep(Duration::from_millis(100)).await;
 
-//     // leave some time to start servers
-//     tokio::time::sleep(Duration::from_millis(100)).await;
+    let mock_validator = MockValidator::new(pbs_port)?;
+    info!("Sending get status");
+    let res = mock_validator.do_get_status().await.expect("failed to get status");
+    assert_eq!(res.status(), StatusCode::OK);
 
-//     let mock_validator = MockValidator::new(pbs_port)?;
-//     info!("Sending get status");
-//     let res = mock_validator.do_get_status().await.expect("failed to get status");
-//     assert_eq!(res.status(), StatusCode::BAD_GATEWAY); // 502 error
+    // Expect two statuses since two relays in config
+    assert_eq!(mock_state.received_get_status(), 2);
+    Ok(())
+}
 
-//     // Expect no statuses since relay is down
-//     assert_eq!(mock_state.received_get_status(), 0);
-//     Ok(())
-// }
+#[tokio::test]
+async fn test_get_status_returns_502_if_relay_down_deneb() -> Result<()> {
+    setup_test_env();
+    let signer = random_secret();
+    let pubkey: BlsPublicKey = blst_pubkey_to_alloy(&signer.sk_to_pk()).into();
+
+    let chain = Chain::Holesky;
+    let pbs_port = 3600;
+    let relay_port = pbs_port + 1;
+
+    let relays = vec![generate_mock_relay(relay_port, *pubkey)?];
+    let mock_state = Arc::new(MockRelayState::new(chain, signer));
+
+    // Don't start the relay
+    // tokio::spawn(start_mock_relay_service(mock_state.clone(), relay_port));
+
+    let config = to_pbs_config(chain, get_pbs_static_config(pbs_port), relays.clone());
+    let state = PbsState::new(config);
+    tokio::spawn(PbsService::run::<(), DenebSpec, DefaultBuilderApi>(state));
+
+    // leave some time to start servers
+    tokio::time::sleep(Duration::from_millis(100)).await;
+
+    let mock_validator = MockValidator::new(pbs_port)?;
+    info!("Sending get status");
+    let res = mock_validator.do_get_status().await.expect("failed to get status");
+    assert_eq!(res.status(), StatusCode::BAD_GATEWAY); // 502 error
+
+    // Expect no statuses since relay is down
+    assert_eq!(mock_state.received_get_status(), 0);
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_get_status_returns_502_if_relay_down_electra() -> Result<()> {
+    setup_test_env();
+    let signer = random_secret();
+    let pubkey: BlsPublicKey = blst_pubkey_to_alloy(&signer.sk_to_pk()).into();
+
+    let chain = Chain::Holesky;
+    let pbs_port = 3650;
+    let relay_port = pbs_port + 1;
+
+    let relays = vec![generate_mock_relay(relay_port, *pubkey)?];
+    let mock_state = Arc::new(MockRelayState::new(chain, signer));
+
+    // Don't start the relay
+    // tokio::spawn(start_mock_relay_service(mock_state.clone(), relay_port));
+
+    let config = to_pbs_config(chain, get_pbs_static_config(pbs_port), relays.clone());
+    let state = PbsState::new(config);
+    tokio::spawn(PbsService::run::<(), ElectraSpec, DefaultBuilderApi>(state));
+
+    // leave some time to start servers
+    tokio::time::sleep(Duration::from_millis(100)).await;
+
+    let mock_validator = MockValidator::new(pbs_port)?;
+    info!("Sending get status");
+    let res = mock_validator.do_get_status().await.expect("failed to get status");
+    assert_eq!(res.status(), StatusCode::BAD_GATEWAY); // 502 error
+
+    // Expect no statuses since relay is down
+    assert_eq!(mock_state.received_get_status(), 0);
+    Ok(())
+}
