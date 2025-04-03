e7f22e8a6cb2985c6c5bbe50155ef8cab3171b79
WillQ <zsluedem06@gmail.com>
2025-04-03 22:18:15 +0800

    chore: expose electra struct

--- crates/common/src/pbs/types/mod.rs
+++ crates/common/src/pbs/types/mod.rs
@@ -17,12 +17,15 @@ pub use execution_payload::{
     ExecutionPayload, ExecutionPayloadHeader, Transaction, Transactions, Withdrawal,
     EMPTY_TX_ROOT_HASH,
 };
+pub use execution_requests::{
+    ConsolidationRequest, DepositRequest, ExecutionRequests, WithdrawalRequest,
+};
 pub use get_header::{
-    ExecutionPayloadHeaderMessageDeneb, GetHeaderParams, GetHeaderResponse,
-    SignedExecutionPayloadHeader,
+    ExecutionPayloadHeaderMessageDeneb, ExecutionPayloadHeaderMessageElectra, GetHeaderParams,
+    GetHeaderResponse, SignedExecutionPayloadHeader,
 };
 pub use kzg::{
     KzgCommitment, KzgCommitments, KzgProof, KzgProofs, BYTES_PER_COMMITMENT, BYTES_PER_PROOF,
 };
-pub use spec::{DenebSpec, EthSpec};
+pub use spec::{DenebSpec, ElectraSpec, EthSpec};
 pub use utils::VersionedResponse;
