0e1b9d452384cc4fa48e13e80bbd64b64ccb9483
Joe Clapis <jclapis@outlook.com>
2025-07-30 15:29:55 -0400

    Chore: cleanup some signer consts (#346)

--- crates/common/src/signer/constants.rs
+++
@@ -1,6 +0,0 @@
-pub const DEFAULT_SIGNER_PORT: u16 = 20000;
-
-// Rate limit signer API requests for 5 minutes after the endpoint has 3 JWT
-// auth failures
-pub const DEFAULT_JWT_AUTH_FAIL_LIMIT: u32 = 3;
-pub const DEFAULT_JWT_AUTH_FAIL_TIMEOUT_SECONDS: u32 = 5 * 60;
