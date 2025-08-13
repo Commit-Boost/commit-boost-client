dcd3daead42f86d5a309b48d5fb29e19499d7c47
ltitanb <163874448+ltitanb@users.noreply.github.com>
2025-08-13 17:31:47 +0100

    chore: more logs in pbs module (#359)

--- crates/pbs/src/mev_boost/get_header.rs
+++ crates/pbs/src/mev_boost/get_header.rs
@@ -328,6 +328,7 @@ async fn send_one_get_header(
     RELAY_STATUS_CODE.with_label_values(&[code.as_str(), GET_HEADER_ENDPOINT_TAG, &relay.id]).inc();
 
     let response_bytes = read_chunked_body_with_max(res, MAX_SIZE_GET_HEADER_RESPONSE).await?;
+    let header_size_bytes = response_bytes.len();
     if !code.is_success() {
         return Err(PbsError::RelayResponse {
             error_msg: String::from_utf8_lossy(&response_bytes).into_owned(),
@@ -357,6 +358,7 @@ async fn send_one_get_header(
 
     debug!(
         relay_id = relay.id.as_ref(),
+        header_size_bytes,
         latency = ?request_latency,
         version = get_header_response.version(),
         value_eth = format_ether(get_header_response.value()),
