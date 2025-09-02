52aec57e9d24541adde0439fe378e59f444349be
Joe Clapis <jclapis@outlook.com>
2025-09-01 23:57:21 -0400

    Add payload hash to signer JWT claims (#356)
    Co-authored-by: eltitanb <lorenzo@gattaca.com>
    Co-authored-by: ltitanb <163874448+ltitanb@users.noreply.github.com>
    Co-authored-by: Manuel Iñaki Bilbao <manuel.bilbao@lambdaclass.com>

--- tests/tests/signer_jwt_auth.rs
+++ tests/tests/signer_jwt_auth.rs
@@ -2,13 +2,16 @@ use std::{collections::HashMap, time::Duration};
 
 use alloy::primitives::b256;
 use cb_common::{
-    commit::constants::{GET_PUBKEYS_PATH, REVOKE_MODULE_PATH},
+    commit::{
+        constants::{GET_PUBKEYS_PATH, REVOKE_MODULE_PATH},
+        request::RevokeModuleRequest,
+    },
     config::{load_module_signing_configs, ModuleSigningConfig},
     types::ModuleId,
-    utils::create_jwt,
+    utils::{create_admin_jwt, create_jwt},
 };
 use cb_tests::{
-    signer_service::{create_admin_jwt, start_server, verify_pubkeys},
+    signer_service::{start_server, verify_pubkeys},
     utils::{self, setup_test_env},
 };
 use eyre::Result;
@@ -42,7 +45,7 @@ async fn test_signer_jwt_auth_success() -> Result<()> {
     let jwt_config = mod_cfgs.get(&module_id).expect("JWT config for test module not found");
 
     // Run a pubkeys request
-    let jwt = create_jwt(&module_id, &jwt_config.jwt_secret)?;
+    let jwt = create_jwt(&module_id, &jwt_config.jwt_secret, None)?;
     let client = reqwest::Client::new();
     let url = format!("http://{}{}", start_config.endpoint, GET_PUBKEYS_PATH);
     let response = client.get(&url).bearer_auth(&jwt).send().await?;
@@ -61,7 +64,7 @@ async fn test_signer_jwt_auth_fail() -> Result<()> {
     let start_config = start_server(20101, &mod_cfgs, ADMIN_SECRET.to_string()).await?;
 
     // Run a pubkeys request - this should fail due to invalid JWT
-    let jwt = create_jwt(&module_id, "incorrect secret")?;
+    let jwt = create_jwt(&module_id, "incorrect secret", None)?;
     let client = reqwest::Client::new();
     let url = format!("http://{}{}", start_config.endpoint, GET_PUBKEYS_PATH);
     let response = client.get(&url).bearer_auth(&jwt).send().await?;
@@ -83,7 +86,7 @@ async fn test_signer_jwt_rate_limit() -> Result<()> {
     let mod_cfg = mod_cfgs.get(&module_id).expect("JWT config for test module not found");
 
     // Run as many pubkeys requests as the fail limit
-    let jwt = create_jwt(&module_id, "incorrect secret")?;
+    let jwt = create_jwt(&module_id, "incorrect secret", None)?;
     let client = reqwest::Client::new();
     let url = format!("http://{}{}", start_config.endpoint, GET_PUBKEYS_PATH);
     for _ in 0..start_config.jwt_auth_fail_limit {
@@ -92,7 +95,7 @@ async fn test_signer_jwt_rate_limit() -> Result<()> {
     }
 
     // Run another request - this should fail due to rate limiting now
-    let jwt = create_jwt(&module_id, &mod_cfg.jwt_secret)?;
+    let jwt = create_jwt(&module_id, &mod_cfg.jwt_secret, None)?;
     let response = client.get(&url).bearer_auth(&jwt).send().await?;
     assert!(response.status() == StatusCode::TOO_MANY_REQUESTS);
 
@@ -116,8 +119,7 @@ async fn test_signer_revoked_jwt_fail() -> Result<()> {
     let start_config = start_server(20400, &mod_cfgs, admin_secret.clone()).await?;
 
     // Run as many pubkeys requests as the fail limit
-    let jwt = create_jwt(&module_id, JWT_SECRET)?;
-    let admin_jwt = create_admin_jwt(admin_secret)?;
+    let jwt = create_jwt(&module_id, JWT_SECRET, None)?;
     let client = reqwest::Client::new();
 
     // At first, test module should be allowed to request pubkeys
@@ -125,14 +127,13 @@ async fn test_signer_revoked_jwt_fail() -> Result<()> {
     let response = client.get(&url).bearer_auth(&jwt).send().await?;
     assert!(response.status() == StatusCode::OK);
 
+    let revoke_body = RevokeModuleRequest { module_id: ModuleId(JWT_MODULE.to_string()) };
+    let body_bytes = serde_json::to_vec(&revoke_body)?;
+    let admin_jwt = create_admin_jwt(admin_secret, Some(&body_bytes))?;
+
     let revoke_url = format!("http://{}{}", start_config.endpoint, REVOKE_MODULE_PATH);
-    let response = client
-        .post(&revoke_url)
-        .header("content-type", "application/json")
-        .body(reqwest::Body::wrap(format!("{{\"module_id\": \"{JWT_MODULE}\"}}")))
-        .bearer_auth(&admin_jwt)
-        .send()
-        .await?;
+    let response =
+        client.post(&revoke_url).json(&revoke_body).bearer_auth(&admin_jwt).send().await?;
     assert!(response.status() == StatusCode::OK);
 
     // After revoke, test module shouldn't be allowed anymore
@@ -150,30 +151,21 @@ async fn test_signer_only_admin_can_revoke() -> Result<()> {
     let mod_cfgs = create_mod_signing_configs().await;
     let start_config = start_server(20500, &mod_cfgs, admin_secret.clone()).await?;
 
+    let revoke_body = RevokeModuleRequest { module_id: ModuleId(JWT_MODULE.to_string()) };
+    let body_bytes = serde_json::to_vec(&revoke_body)?;
+
     // Run as many pubkeys requests as the fail limit
-    let jwt = create_jwt(&module_id, JWT_SECRET)?;
-    let admin_jwt = create_admin_jwt(admin_secret)?;
+    let jwt = create_jwt(&module_id, JWT_SECRET, Some(&body_bytes))?;
     let client = reqwest::Client::new();
     let url = format!("http://{}{}", start_config.endpoint, REVOKE_MODULE_PATH);
 
     // Module JWT shouldn't be able to revoke modules
-    let response = client
-        .post(&url)
-        .header("content-type", "application/json")
-        .body(reqwest::Body::wrap(format!("{{\"module_id\": \"{JWT_MODULE}\"}}")))
-        .bearer_auth(&jwt)
-        .send()
-        .await?;
+    let response = client.post(&url).json(&revoke_body).bearer_auth(&jwt).send().await?;
     assert!(response.status() == StatusCode::UNAUTHORIZED);
 
     // Admin should be able to revoke modules
-    let response = client
-        .post(&url)
-        .header("content-type", "application/json")
-        .body(reqwest::Body::wrap(format!("{{\"module_id\": \"{JWT_MODULE}\"}}")))
-        .bearer_auth(&admin_jwt)
-        .send()
-        .await?;
+    let admin_jwt = create_admin_jwt(admin_secret, Some(&body_bytes))?;
+    let response = client.post(&url).json(&revoke_body).bearer_auth(&admin_jwt).send().await?;
     assert!(response.status() == StatusCode::OK);
 
     Ok(())
